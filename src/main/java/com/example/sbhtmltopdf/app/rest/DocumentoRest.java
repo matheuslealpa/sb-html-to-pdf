16471d2457aae92f4a1a2b559c8bf9be2157d1d9
Matheus Leal <matheuslealpa@gmail.com>
2023-05-14 13:29:43 -0300

    feat: crud da entidade de documento

--- src/main/java/com/example/sbhtmltopdf/app/rest/DocumentoRest.java
+++ src/main/java/com/example/sbhtmltopdf/app/rest/DocumentoRest.java
@@ -3,18 +3,23 @@ package com.example.sbhtmltopdf.app.rest;
 import com.example.sbhtmltopdf.app.domain.Documento;
 import com.example.sbhtmltopdf.app.repository.DocumentoRepository;
 import com.example.sbhtmltopdf.app.service.HtmlToPdfConverterService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
 @RestController
+@AllArgsConstructor
 @RequestMapping("api/documento")
 public class DocumentoRest {
-    @Autowired
-    private DocumentoRepository documentoRepository;
-    @Autowired
-    private HtmlToPdfConverterService htmlToPDF;
+
+    private final DocumentoRepository documentoRepository;
+    private final HtmlToPdfConverterService htmlToPDF;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -24,4 +29,35 @@ public class DocumentoRest {
         }
         return ResponseEntity.ok(documentoRepository.save(resource));
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Page<Documento>> findALl(Pageable pageable){
+        return ResponseEntity.ok(documentoRepository.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Optional<Documento>> findById(@PathVariable Long id){
+        if (!documentoRepository.existsById(id)) throw new EntityNotFoundException("Entidade não encontrada");
+        return ResponseEntity.ok(documentoRepository.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Documento> update(@PathVariable Long id, @RequestBody Documento resource){
+        if (!documentoRepository.existsById(id)) throw new EntityNotFoundException("entidade não encontrada");
+        if (resource.getDocumentPDF() !=null){
+            resource.setDocumentPDF(htmlToPDF.exec(resource.getDocumentPDF()));
+        }
+        return ResponseEntity.ok(documentoRepository.save(resource));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id, Documento resource){
+        if (!documentoRepository.existsById(id)) throw new EntityNotFoundException("entidade não encontrada");
+        documentoRepository.delete(resource);
+    }
+
 }
